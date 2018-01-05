cf216ae0eadc22a37fca8e70972e9ba052914d41
yuguorui <yuguorui96@gmail.com>
2018-01-05 09:36:19 +0800

    发现了新的错误，小卒子过河仍不能拐弯，运行单元测试即可发现。

--- test_script/BoardUnitTest.py
+++ test_script/BoardUnitTest.py
@@ -15,11 +15,20 @@ class BoardUnitTest(unittest.TestCase):
     def test_pawn1(self):
         fen = '9/9/9/9/9/9/P1P1P1P1P/9/9/9 w - - 0 1'
         board = Board(fen)
-        corrent_set = {'a3a4', 'c3c4', 'e3e4', 'g3g4', 'i3i4'}
+        correct_set = {'a3a4', 'c3c4', 'e3e4', 'g3g4', 'i3i4'}
         result_set = set()
         for step in board.generate_legal_moves():
             result_set.add(step.ucci())
-        self.assertEqual(corrent_set, result_set)
+        self.assertEqual(correct_set, result_set)
+
+    def test_pawn2(self):
+        fen = '9/9/9/9/4P4/9/P1P3P1P/9/9/9 w - - 0 1'
+        board = Board(fen)
+        correct_set = {'a3a4', 'c3c4', 'e5d5', 'e5f5', 'e5e6', 'g3g4', 'i3i4'}
+        result_set = set()
+        for step in board.generate_legal_moves():
+            result_set.add(step.ucci())
+        self.assertEqual(correct_set, result_set)
 
     def test_fen(self):
         fen = 'r1bakabn1/9/1cn4cr/p1p1p3p/6p2/9/P1P1P1P1P/1C2C1N2/9/RNBAKABR1 w - - 0 4'
